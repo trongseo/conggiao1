56d7f588a8f15c9eafce8be79565425aea09b420
trongseo <trongseo@gmail.com>
2015-07-24 23:48:05 +0700

    zz

---
+++ DB_CONFIG.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.  'connectionString' => 'mysql:host=chuyennhat.vn;dbname=test_glass1',
+'emulatePrepare' => true,
+'username' => 'test_glass1',
+'password' => '123456789',
+'charset' => 'utf8',
+'schemaCachingDuration' => 60 * 60,
+ * User: PHAT
+ * Date: 4/19/15$db_host = "INSERT_DB_HOST_HERE";
+$db_name = "INSERT_DB_NAME_HERE";
+$db_user = "INSERT_DB_USERNAME_HERE";
+$db_pw = "INSERT_DB_PASSWORD_HERE";
+ * Time: 5:16 PM
+ */
+//define("DB_HOST","chuyennhat.vn");
+//define("DB_NAME","test_glass1");
+//define("DB_USERNAME","test_glass1");
+//define("DB_PASSWORD","123456789");
+define("SMTP_HOST","mail.kinhtanphuc.com");
+define("ADMIN_EMAIL","info@kinhtanphuc.com");
+define("SMTP_USERNAME","info@kinhtanphuc.com");
+define("SMTP_PASSWORD","123456");
+define("SMTP_INCLUDED",1);
+
+//define("DB_HOST","localhost");
+//define("DB_NAME","kinhtanp_db");
+//define("DB_USERNAME","kinhtanp_db");
+//define("DB_PASSWORD","!}8up6K320c(");
+
+//define("DB_HOST","localhost");
+//define("DB_NAME","kinhtanp_db");
+//define("DB_USERNAME","root");
+//define("DB_PASSWORD","123456");
+
+define("DB_HOST","localhost");
+define("DB_NAME","1thuvienconggiao");
+define("DB_USERNAME","root");
+define("DB_PASSWORD","123456");
